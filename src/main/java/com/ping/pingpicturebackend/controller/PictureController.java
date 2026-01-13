7cb686f3c2113e25ade42a527dba8935d4bf3806
qiuping01 <2187718660@qq.com>
2026-01-13 20:25:03 +0800

    feat(picture): 添加批量编辑图片功能

--- src/main/java/com/ping/pingpicturebackend/controller/PictureController.java
+++ src/main/java/com/ping/pingpicturebackend/controller/PictureController.java
@@ -361,4 +361,16 @@ public class PictureController {
         List<PictureVO> pictureVOList = pictureService.searchPictureByColor(picColor, spaceId, loginUser);
         return ResultUtils.success(pictureVOList);
     }
+
+    /**
+     * 批量编辑图片
+     */
+    @PostMapping("/edit/batch")
+    public BaseResponse<Boolean> editPictureByBatch(@RequestBody PictureEditByBatchRequest pictureEditByBatchRequest,
+                                                    HttpServletRequest request) {
+        ThrowUtils.throwIf(pictureEditByBatchRequest == null, ErrorCode.PARAMS_ERROR);
+        User loginUser = userService.getLoginUser(request);
+        pictureService.editPictureByBatch(pictureEditByBatchRequest, loginUser);
+        return ResultUtils.success(true);
+    }
 }
