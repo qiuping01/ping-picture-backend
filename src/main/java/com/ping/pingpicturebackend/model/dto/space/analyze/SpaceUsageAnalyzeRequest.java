2ca5205e52940e375e59ef3181b18c9891170311
qiuping01 <2187718660@qq.com>
2026-01-16 23:40:59 +0800

    feat(space): 添加空间使用资源分析

---
+++ src/main/java/com/ping/pingpicturebackend/model/dto/space/analyze/SpaceUsageAnalyzeRequest.java
@@ -0,0 +1,13 @@
+package com.ping.pingpicturebackend.model.dto.space.analyze;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 空间资源使用情况分析请求
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SpaceUsageAnalyzeRequest extends SpaceAnalyzeRequest {
+
+}
