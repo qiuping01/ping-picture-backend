9081a5d00db9fe18dc09d75f2cdb23e2d60846bd
qiuping01 <2187718660@qq.com>
2026-01-30 01:54:57 +0800

    feat(spaceUser): 添加从请求中获取上下文对象

--- src/main/java/com/ping/pingpicturebackend/manager/auth/satoken/StpInterfaceImpl.java
+++ src/main/java/com/ping/pingpicturebackend/manager/auth/satoken/StpInterfaceImpl.java
@@ -2,12 +2,25 @@ package com.ping.pingpicturebackend.manager.auth.satoken;
 
 import cn.dev33.satoken.stp.StpInterface;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.http.ContentType;
+import cn.hutool.http.Header;
+import cn.hutool.json.JSONUtil;
+import com.ping.pingpicturebackend.manager.auth.SpaceUserAuthContext;
 import com.ping.pingpicturebackend.model.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static com.ping.pingpicturebackend.constant.UserConstant.USER_LOGIN_STATE;
 
@@ -30,5 +43,51 @@ public class StpInterfaceImpl implements StpInterface {
         // 从当前登录用户信息中获取角色
         User user = (User) StpUtil.getSessionByLoginId(loginId).get(USER_LOGIN_STATE);
         return Collections.singletonList(user.getUserRole());
+    }
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    /**
+     * 从请求中获取上下文对象
+     */
+    private SpaceUserAuthContext getAuthContextByRequest() {
+        // 获取请求对象
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        // 获取请求类别 get / post
+        String contentType = request.getHeader(Header.CONTENT_TYPE.getValue());
+        // 对应封装结果
+        SpaceUserAuthContext authRequest;
+        if ((ContentType.JSON.getValue().equals(contentType))) {
+            // post 请求
+            String body = ServletUtil.getBody(request);
+            authRequest = JSONUtil.toBean(body, SpaceUserAuthContext.class);
+        } else {
+            // get 请求
+            Map<String, String> paramMap = ServletUtil.getParamMap(request);
+            authRequest = BeanUtil.toBean(paramMap, SpaceUserAuthContext.class);
+        }
+        // 根据请求路径区分 id 字段的含义
+        Long id = authRequest.getId();
+        if (ObjUtil.isNotNull(id)) {
+            String requestURI = request.getRequestURI();
+            // 替换掉 api 上下文 "/api/"  → ""
+            String pathURI = requestURI.replace(contextPath + "/", "");
+            // 获取前缀的第一个斜杠前的字符串
+            String moduleName = StrUtil.subBefore(pathURI, "/", false);
+            switch (moduleName) {
+                case "picture":
+                    authRequest.setPictureId(id);
+                    break;
+                case "space":
+                    authRequest.setSpaceId(id);
+                    break;
+                case "spaceUser":
+                    authRequest.setSpaceUserId(id);
+                    break;
+                default:
+            }
+        }
+        return authRequest;
     }
 }
